2359b81c6c3147b5540fa85e470e2d515d4cf363
sephclt <josephajero22@gmail.com>
2024-11-20 22:15:08 +0800

    feedback page finished

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,18 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { getFirestore, collection, addDoc } from "firebase/firestore";
+import { db } from "../firebase/config";
 
 export default function Feedback() {
-  const [feedback, setFeedback] = useState('');
+  const [feedback, setFeedback] = useState("");
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    alert(`Feedback Submitted: ${feedback}`);
-    setFeedback('');
+    try {
+      await addDoc(collection(db, "feedbacks"), {
+        message: feedback,
+      });
+      alert(`Feedback Submitted: ${feedback}`);
+      setFeedback("");
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <h2>Feedback</h2>
-      <textarea value={feedback} onChange={(e) => setFeedback(e.target.value)} required></textarea>
+      <textarea
+        value={feedback}
+        onChange={(e) => setFeedback(e.target.value)}
+        required
+      ></textarea>
       <button type="submit">Submit Feedback</button>
     </form>
   );
